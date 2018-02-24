7774d528db0dc993d3c6d9a0bd53f5ee0915af6e
Marco Grassia <marco.grassia@studium.unict.it>
2018-02-24 17:15:38 +0100

    [WIP] Circuit Configurator

--- src/network-simulator/Circuit_configurator.m
+++ src/network-simulator/Circuit_configurator.m
@@ -1,97 +1,104 @@
 % CONFIG BEGIN
+zones_num = 3;
+period = 10; % seconds
 
 vasca_1_c    = 10;
 vasca_1_r    = 100;
-vasca_1_r_in = 100;
-vasca_1_sw   = 1;
+vasca_1_r_in = single(100);
+vasca_1_sw   = uint8(1);
 
 vasca_2_c    = 10;
 vasca_2_r    = 100;
-vasca_2_r_in = 100;
-vasca_2_sw   = 1;
+vasca_2_r_in = single(100);
+vasca_2_sw   = uint8(1);
 
 vasca_3_c    = 10;
 vasca_3_r    = 100;
-vasca_3_r_in = 100;
-vasca_3_sw   = 1;
+vasca_3_r_in = single(100);
+vasca_3_sw   = uint8(1);
 
 v_pompa      = 1600;
 r_pompa      = 100;
 
 % CONFIG END
 
+addpath RealTime_Pacer/
 
+% float capacity;
+% float inputFlowRate;
+% float outputFlowRate;
+% float valve;
+% bool schmidtTrigger;
 
-% addpath RealTime_Pacer/
+model_name = 'Rete'
+zone_name = 'Vasca%d'
+read_elements = ['Voltimeter/v_vasca', 'iFlowRate', 'flowRate', 'Variable Resistor', 'Switch/sw_vasca']
 
+% zone = sprintf(zone_name, i);
+% sprintf('%s/%s/%s', model_name, zone, param_name);
 
-i = 0;
-
+% i = 0;
 % set_param('elementary/v1_c_switch', 'value', 'i');
 
-
-
-% 
 % % function a = test
 % a = timer('Execution Mode', 'Fixed rate', 'Period', 1, 'TimerFcn', @myFun, 'TaskToSchedule', 10);
 
 myMQTT = mqtt('tcp://localhost:1883');
 
+subscriptions = containers.Map('KeyType', 'int32', 'ValueType', 'any');
+publish_urls = containers.Map('KeyType', 'int32', 'ValueType', 'any');
 
-mySub = subscribe(myMQTT,'myMQTT')
+for i = 1:zones_num
+  tmp = struct('Sub', subscribe(myMQTT, sprintf('/actuators/zones/%d/', i)));
+  subscriptions(i) = tmp;
 
-
-while 1
-    
-%     vasca_1_c    = 10;
-%     vasca_1_r    = 100;
-%     vasca_1_r_in = 100;
-%     vasca_1_sw   = 1;
-% 
-%     vasca_2_c    = 10;
-%     vasca_2_r    = 100;
-%     vasca_2_r_in = 100;
-%     vasca_2_sw   = 1;
-% 
-%     vasca_3_c    = 10;
-%     vasca_3_r    = 100;
-%     vasca_3_r_in = 100;
-%     vasca_3_sw   = 1;
-    
-    
-    
+  publish_urls(i) = sprintf('/sensors/zones/${i}/', i);
 end
 
 
 
-function myFig = createFigure() 
-    figure
-    subplot(3,2,1)
-    title('Vasca (Tensione)')
-    plot(vasca1.time, vasca1.data(:,1))
-    ylabel('V')
-    
-    subplot(3,2,2)
-    title('Vasca (Correnti)')
-    ylabel('A')
-    plot(vasca1.time, vasca1.data(:,2:3))
-    subplot(3,2,3)
-    
-    title(string('Pozzo (Tensione)'))
-    ylabel('V')
-    plot(pozzo.time, pozzo.data(:,1))
-    
-    subplot(3,2,4)
-    title(string('Pozzo (Correnti)'))
-    ylabel('A')
-    plot(pozzo.time, pozzo.data(:,2:3))
-    
-    subplot(3,2,5)
-    xlabel('s')
-    plot(utilizator.time, (utilizator.Data/(10^9)))
-    
-    subplot(3,2,6)
-    title(string('Corrente utilizzatore'))
-    plot(utilizatorFlowRate)
-    
+
+while 1
+    try
+      % publish(myMQTT, 'myMQTT', 'testMessage');
+      % pause(1);
+      % test = read(mySub);
+      readings = {}
+
+      for k=1:length(subscriptions)
+        subscription=subscriptions{k};
+        readings{k} = read(mySub);
+      end
+
+    catch
+    end
+
+    pause(period);
+end
+
+
+
+function y = actuate(bits)
+  y = fi(varargin);
+end
+
+
+function y = concatb(varargin)
+  y = fi(varargin);
+end
+
+function y = single2b(x)
+   y = de2bi(typecast(single(x), 'uint32'), 32);
+end
+
+function y = boolean2b(x)
+   y = de2bi(typecast(single(x), 'uint8'), 8);
+end
+
+function y = b2single(x)
+  y = typecast(bi2de(tmp), 'single')
+end
+
+function y = b2boolean(x)
+  y = typecast(bi2de(tmp), 'uint8')
 end
