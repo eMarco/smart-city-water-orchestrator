8db3b101b1ec0909eeeab86cec64a005c8e53aee
aleskandro <aleskandro@lucylaika.ovh>
2018-02-26 12:43:37 +0100

    Matlab script refactoring

--- src/network-simulator/Circuit_configurator.m
+++ src/network-simulator/Circuit_configurator.m
@@ -1,8 +1,18 @@
 % CONFIG BEGIN
-zones_num = 3;
-period = 10; % seconds
+addpath RealTime_Pacer/
 
-vasca_sw   = uint8(1);
+% STARTING MQTT (THE NEXT COMMAND WILL CRASH IF DOCKER (OR THE BROKER) IS
+% NOT RUNNING
+try
+    myMQTT = mqtt('tcp://localhost:1883');
+catch
+    disp("Unable to create MQTT Connection. Is broker reacheable and available?");
+end
+
+zones_num = 3;
+period    = 10; % seconds
+
+vasca_sw     = uint8(1);
 vasca_r_in   = single(100);
 
 vasca_1_c    = 10;
@@ -16,10 +26,6 @@ vasca_3_r    = 100;
 
 v_pompa      = 1600;
 r_pompa      = 100;
-
-
-values = string([ vasca_r_in, vasca_sw ]);
-
 
 model_name = 'Rete';
 zone_name = 'Vasca%d';
@@ -36,16 +42,19 @@ write_elements = [
       struct('Name', 'Vasca%d/trigger/vasca_sw', 'Type', 'uint8'),
 ];
 
-
-% CONFIG END
-
 classes = {'double','single','int8','uint8','int16','uint16','int32','uint32','int64','uint64'};
 sizes = [8,4,1,1,2,2,4,4,8,8];
 handles2b = {0, @single2b, 0, @boolean2b, 0, 0, 0, 0, 0, 0};
 handles2v = {0, @b2single, 0, @b2boolean, 0, 0, 0, 0, 0, 0};
+% CONFIG END 
 
-addpath RealTime_Pacer/
+% OPEN MODEL AND START SIMULATION
 
+open_system(model_name);
+set_param(model_name,'SimulationCommand','start')
+
+% RESET THE MODEL (SIMULATION MUST BE RUNNING)
+values = string([ vasca_r_in, vasca_sw ]);
 for i=1:zones_num
     for k=1:(length(write_elements))
         tmp_index = find(strcmp(classes, write_elements(k).Type));
@@ -57,7 +66,7 @@ for i=1:zones_num
     end
 end
 
-myMQTT = mqtt('tcp://localhost:1883');
+% CREATING SUBSCRIPTIONS AND PUBLISH_URLS
 
 subscriptions = containers.Map('KeyType', 'int32', 'ValueType', 'any');
 publish_urls = containers.Map('KeyType', 'int32', 'ValueType', 'any');
@@ -69,98 +78,62 @@ for i = 1:zones_num
   publish_urls(i) = sprintf('/sensors/zones/%d/', i);
 end
 
+% INFINITE LOOP TO HANDLE MQTT COMMUNICATION
+
 while 1
-    % PUBLISH
     for i=1:zones_num
-      % readings = {};
+        
+      % PUBLISH
       readings = "";
       for k=1:length(read_elements)
-
-        element = sprintf('%s/%s', model_name, sprintf(read_elements(k).Name, i));
-
-        tmp = get_param(element, 'RuntimeObject');
-        tmp = tmp.InputPort(read_elements(k).Port).Data;
-
-        readings = sprintf('%s%s|', readings, num2str(tmp));
-        % % Get type index
-        % tmp_index = find(strcmp(classes, read_elements(k).Type));
-        %
-        % % Retrieve serialization function
-        % f = handles2b(tmp_index);
-        % f = cell2mat(f);
-        % display(read_elements(k).Name);
-        % display(tmp);
-        % display(f(tmp));
-        % % Append the binary data
-        % readings{k} = f(tmp);
+            element = sprintf('%s/%s', model_name, sprintf(read_elements(k).Name, i));
+            tmp = get_param(element, 'RuntimeObject');
+            tmp = tmp.InputPort(read_elements(k).Port).Data;
+            readings = sprintf('%s%s|', readings, num2str(tmp));
       end
-      %try
-        % display(cell2mat(readings))
-        %display(readings)
-        publish(myMQTT, publish_urls(i), readings);
-      %catch
-      %end
-
+      publish(myMQTT, publish_urls(i), readings);
       % PUBLISH end
 
       % READ
       try
-        % bytes = uint8(read(subscriptions(i).Sub));
         values = strsplit(read(subscriptions(i).Sub), '|');
-        display(values);
-        %
-        % % start_byte = 0;
-        % % for k=length(write_elements):-1:1
+        %%%display(values);
         for k=1:(length(write_elements))
             % Get type index
             tmp_index = find(strcmp(classes, write_elements(k).Type));
-            %
-            % %   % Calculate end bit on size basis
-            % %   end_byte = start_byte + sizes(tmp_index) - 1;
-            % %   tmp = bytes(start_byte:end_byte);
-            %
-            %   % Get i-th element
             tmp = values(k);
-            %
-            %   % Retrieve deserilization function
+            % Retrieve deserilization function
             f = cell2mat(handles2v(tmp_index));
-            %
-            %   % Convert to value
+            % Convert to the proper value
             tmp = f(tmp);
-            %
             element = sprintf('%s/%s', model_name, sprintf(write_elements(k).Name, i));
-            display(tmp);
+            %%%display(tmp);
             set_param(element, 'value', tmp);
-            %
-            % %   start_byte = end_bit + 1;
         end
       catch
       end
-      % %READ END
+      %READ END
     end
 
     pause(period);
 end
 
 function y = concatb(varargin)
-  y = fi(cell2mat(varargin{1,:}));
+    y = fi(cell2mat(varargin{1,:}));
 end
 
 function y = single2b(x)
-   y = typecast(single(x), 'uint16');
+    y = typecast(single(x), 'uint16');
 end
 
 function y = boolean2b(x)
-   y = typecast(single(x), 'uint8');
+    y = typecast(single(x), 'uint8');
 end
 
 function y = b2single(x)
-  %y = single(x);
-  y = sprintf('single(%s)',x);
+    y = sprintf('single(%s)',x);
 end
 
 function y = b2boolean(x)
     y = sprintf('uint8(%s)',x);
- % y = uint8(x);
 end
-
