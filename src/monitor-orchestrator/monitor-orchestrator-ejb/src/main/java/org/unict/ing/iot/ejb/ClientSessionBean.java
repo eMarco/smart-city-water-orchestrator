afde2194b32fb822819e859093db1744c602c0b2
Marco Grassia <marco.grassia@studium.unict.it>
2018-02-26 10:48:29 +0100

    Add ClientSessionBean

---
+++ src/monitor-orchestrator/monitor-orchestrator-ejb/src/main/java/org/unict/ing/iot/ejb/ClientSessionBean.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2018 Marco Grassia <marco.grassia@studium.unict.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.unict.ing.iot.ejb;
+
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import org.unict.ing.iot.utils.model.GenericValue;
+import org.unict.ing.iot.utils.mongodriver.DBConnectionSingletonSessionBeanLocal;
+
+/**
+ *
+ * @author Marco Grassia <marco.grassia@studium.unict.it>
+ */
+@Stateless
+public class ClientSessionBean implements ClientSessionBeanRemote {
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+    @EJB
+    private DBConnectionSingletonSessionBeanLocal db;
+
+    @Override
+    public List<GenericValue> findByClassName(String name) {
+        return this.db.getStorage().findByClassName(name);
+    }
+
+    @Override
+    public List<GenericValue> findByClassNameAndFieldMatch(String name, String field, String value) {
+        return this.db.getStorage().findByClassNameAndFieldMatch(name, field, value);
+    }
+}
---
+++ src/monitor-orchestrator/utils/src/main/java/org/unict/ing/iot/ejb/ClientSessionBeanRemote.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2018 Marco Grassia <marco.grassia@studium.unict.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.unict.ing.iot.ejb;
+
+import java.util.List;
+import javax.ejb.Remote;
+import org.unict.ing.iot.utils.model.GenericValue;
+
+/**
+ *
+ * @author Marco Grassia <marco.grassia@studium.unict.it>
+ */
+@Remote
+public interface ClientSessionBeanRemote {
+    public List<GenericValue> findByClassName(String name);
+    public List<GenericValue> findByClassNameAndFieldMatch(String name, String field, String value);
+}
