12b157e48955a0af8152ce93414346ec4298091e
Marco Grassia <marco.grassia@studium.unict.it>
2018-02-27 10:09:16 +0100

    Circuit Configurator: Sectors

--- src/network-simulator/Circuit_configurator.m
+++ src/network-simulator/Circuit_configurator.m
@@ -10,6 +10,13 @@ catch
 end
 
 zones_num = 3;
+sectors_per_zone = [ 1, 1, 1 ];
+
+% Sector
+% flowRate = flowRateTotSector%d_%d
+% flowRateCounted = flowRate%d_%d
+% trigger = triggerStatusSector%d_%d
+
 period    = 10; % seconds
 
 vasca_sw     = uint8(1);
@@ -29,7 +36,7 @@ r_pompa      = 100;
 
 model_name = 'Rete';
 zone_name = 'Vasca%d';
-read_elements = [
+zone_read_elements = [
       struct('Name', 'v_vasca_%d', 'Type', 'single', 'Port', 1),
       struct('Name', 'i_vasca_%d', 'Type', 'single', 'Port', 1),
       struct('Name', 'i_vasca_%d', 'Type', 'single', 'Port', 2),
@@ -37,16 +44,22 @@ read_elements = [
       struct('Name', 's_vasca_%d', 'Type', 'uint8', 'Port', 1),
 ];
 
-write_elements = [
+zone_write_elements = [
       struct('Name', 'Vasca%d/vasca_r_in', 'Type', 'single'),
       struct('Name', 'Vasca%d/trigger/vasca_sw', 'Type', 'uint8'),
+];
+
+sector_read_elements = [
+      struct('Name', 'flowRateTotSector%d_%d', 'Type', 'single', 'Port', 1),
+      struct('Name', 'flowRate%d_%d', 'Type', 'single', 'Port', 1),
+      struct('Name', 'triggerStatusSector%d_%d', 'Type', 'uint8', 'Port', 1),
 ];
 
 classes = {'double','single','int8','uint8','int16','uint16','int32','uint32','int64','uint64'};
 sizes = [8,4,1,1,2,2,4,4,8,8];
 handles2b = {0, @single2b, 0, @boolean2b, 0, 0, 0, 0, 0, 0};
 handles2v = {0, @b2single, 0, @b2boolean, 0, 0, 0, 0, 0, 0};
-% CONFIG END 
+% CONFIG END
 
 % OPEN MODEL AND START SIMULATION
 
@@ -56,11 +69,11 @@ set_param(model_name,'SimulationCommand','start');
 % RESET THE MODEL (SIMULATION MUST BE RUNNING)
 values = string([ vasca_r_in, vasca_sw ]);
 for i=1:zones_num
-    for k=1:(length(write_elements))
-        tmp_index = find(strcmp(classes, write_elements(k).Type));
+    for k=1:(length(zone_write_elements))
+        tmp_index = find(strcmp(classes, zone_write_elements(k).Type));
         tmp = values(k);
         f = cell2mat(handles2v(tmp_index));
-        element = sprintf('%s/%s', model_name, sprintf(write_elements(k).Name, i));
+        element = sprintf('%s/%s', model_name, sprintf(zone_write_elements(k).Name, i));
         tmp=f(tmp);
         set_param(element, 'value', tmp);
     end
@@ -75,38 +88,53 @@ for i = 1:zones_num
   tmp = struct('Sub', subscribe(myMQTT, sprintf('/actuators/zones/%d/', i)));
   subscriptions(i) = tmp;
 
-  publish_urls(i) = sprintf('/sensors/zones/%d/', i);
+  tmp = containers.Map('KeyType', 'int32', 'ValueType', 'any');
+  for s = 1:sectors_per_zone(i)
+    tmp(s) = sprintf('/sensors/zones/%d/sectors/%d/', i)
+  end
+  publish_urls(i) = struct('Zone', sprintf('/sensors/zones/%d/', i), 'Sectors', tmp);
 end
 
 % INFINITE LOOP TO HANDLE MQTT COMMUNICATION
 
 while 1
     for i=1:zones_num
-        
-      % PUBLISH
+
+      % PUBLISH Zone infos
       readings = "";
-      for k=1:length(read_elements)
-            element = sprintf('%s/%s', model_name, sprintf(read_elements(k).Name, i));
+      for k=1:length(zone_read_elements)
+            element = sprintf('%s/%s', model_name, sprintf(zone_read_elements(k).Name, i));
             tmp = get_param(element, 'RuntimeObject');
-            tmp = tmp.InputPort(read_elements(k).Port).Data;
+            tmp = tmp.InputPort(zone_read_elements(k).Port).Data;
             readings = sprintf('%s%s|', readings, num2str(tmp));
       end
-      publish(myMQTT, publish_urls(i), readings);
+      publish(myMQTT, publish_urls(i).Zone, readings);
+
+      % PUBLISH Sectors infos
+      readings = "";
+      for k=1:length(zone_read_elements)
+            element = sprintf('%s/%s', model_name, sprintf(zone_read_elements(k).Name, i));
+            tmp = get_param(element, 'RuntimeObject');
+            tmp = tmp.InputPort(zone_read_elements(k).Port).Data;
+            readings = sprintf('%s%s|', readings, num2str(tmp));
+      end
+      publish(myMQTT, publish_urls(i).Zone, readings);
+
       % PUBLISH end
 
       % READ
       try
         values = strsplit(read(subscriptions(i).Sub), '|');
         %%%display(values);
-        for k=1:(length(write_elements))
+        for k=1:(length(zone_write_elements))
             % Get type index
-            tmp_index = find(strcmp(classes, write_elements(k).Type));
+            tmp_index = find(strcmp(classes, zone_write_elements(k).Type));
             tmp = values(k);
             % Retrieve deserilization function
             f = cell2mat(handles2v(tmp_index));
             % Convert to the proper value
             tmp = f(tmp);
-            element = sprintf('%s/%s', model_name, sprintf(write_elements(k).Name, i));
+            element = sprintf('%s/%s', model_name, sprintf(zone_write_elements(k).Name, i));
             %%%display(tmp);
             set_param(element, 'value', tmp);
         end
