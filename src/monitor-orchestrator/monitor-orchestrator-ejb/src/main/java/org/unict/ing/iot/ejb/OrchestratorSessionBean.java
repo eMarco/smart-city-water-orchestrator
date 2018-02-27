a1fa781f3ab7dd718bfb775fe55c4a0111f60f22
Marco Grassia <marco.grassia@studium.unict.it>
2018-02-27 19:33:11 +0100

    Orchestrator strategy

--- src/monitor-orchestrator/monitor-orchestrator-ejb/src/main/java/org/unict/ing/iot/ejb/OrchestratorSessionBean.java
+++ src/monitor-orchestrator/monitor-orchestrator-ejb/src/main/java/org/unict/ing/iot/ejb/OrchestratorSessionBean.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2018 aleskandro - eMarco - cursedLondor
  *
  * This program is free software: you can redistribute it and/or modify
@@ -52,87 +52,83 @@ public class OrchestratorSessionBean implements OrchestratorSessionBeanLocal {
     private static int counter         = 0;
     private static float VMAX          = 700;
     private static float F_THRESHOLD     = (float)0.05;
-    
+
     private static final Logger LOG = Logger.getLogger(OrchestratorSessionBean.class.getName());
-    
+
     @EJB
     private MonitorSessionBeanRemote monitorSessionBean;
 
     @Resource
     private SessionContext context;
-    
+
     @PostConstruct
     private void init() {
         System.err.println("INITINIT" + this.getClass().getClassLoader().toString());
-        
+
     }
-    
+
     @Schedule(dayOfMonth = "*", hour = "*", second = "*/" + PERIOD * ZONE_MULT, minute = "*")
     private void tankActuation() {
-        List<GenericValue> tanks = monitorSessionBean.getTanks(); 
+        List<GenericValue> tanks = monitorSessionBean.getTanks();
         System.err.println(JsonHelper.writeList(tanks));
-        float Vtot = 0;
+        float _Vtot = 0, _IOtot = 0, _Ptot = 0;
         for (int i = 0; i < tanks.size(); i++) {
             Tank t = ((Tank)(tanks.get(i)));
-            Vtot += (VMAX - t.getCapacity()) *  t.getOutputFlowRate();
+
+            _Vtot += (VMAX - t.getCapacity());
+            _IOtot += t.getOutputFlowRate();
+            _Ptot += (VMAX - t.getCapacity()) *  t.getOutputFlowRate();
         }
-        final float Vtot2 = Vtot;
-        tanks.forEach(tankk -> { 
-            
+
+        final float Vtot = _Vtot;
+        final float IOtot = _IOtot;
+        final float Ptot = _Ptot;
+
+        tanks.forEach(tankk -> {
+
             if (tankk instanceof Tank) {
                 Tank tank = (Tank) tankk;
                 String log = tank.toString();
-                
-                if (counter % 5 == 0) {
-                    float rj;
-                    float c = (VMAX - tank.getCapacity());
-                    if (c > F_THRESHOLD && tank.getOutputFlowRate() > F_THRESHOLD) {
-                        rj = Vtot2 / (c * tank.getOutputFlowRate());
+
+                float rj = 0;
+                float c = (VMAX - tank.getCapacity());
+                if (c > F_THRESHOLD && tank.getOutputFlowRate() > F_THRESHOLD) {                // 1: Tank NOT FULL    w\      OUTPUT
+
+                        rj = Ptot / (c * tank.getOutputFlowRate());
                         tank.getValve().setFlowRateResistance(rj);
-                        log += " - Setting input resistance to " + rj + " - ";
-                    } else if (tank.getOutputFlowRate() < F_THRESHOLD && c > F_THRESHOLD) { // Full tank
-                        tank.getValve().increment();
-                    } else if (tank.getOutputFlowRate() > F_THRESHOLD && c < F_THRESHOLD) { 
-                        
-                    } else {
-                        
-                    }
-                    
-                    
-                    counter = 1;
-                    
-                } else {
-                    float diff = tank.getInputFlowRate() - tank.getOutputFlowRate();
-                    log += " " + diff;
-                   
-                    if (diff > flowRateError()) {
-                        log += " DECREMENTING (RUBINETT)";
-                        tank.getValve().increment();
-                    } else {
-                        log += " INCREMENTING (RUBINETT)";
-                        tank.getValve().decrement();
-                    }
-                    
-                    counter++;
+
+                // c * tank.getOutputFlowRate() is probably too small
+                } else if (tank.getOutputFlowRate() < F_THRESHOLD && c > F_THRESHOLD) {         // 2: Tank NOT FULL    w\      NO OUTPUT
+                    // Increase RES to yeld on other tanks                                      // RES SMALL increment
+                    tank.getValve().increment(1-(c/Vtot));
+                } else if (tank.getOutputFlowRate() > F_THRESHOLD && c < F_THRESHOLD) {         // 3: FULL Tank        w\      OUTPUT
+                    // Increase RES to yeld on other tanks                                      // RES MEDIUM increment
+                    tank.getValve().increment(1-(tank.getOutputFlowRate()/IOtot));
+                } else /* (tank.getOutputFlowRate() < F_THRESHOLD && c < F_THRESHOLD) */ {      // 4: FULL Tank        w\      NO OUTPUT
+                    // Increase RES to yeld on other tanks                                      // RES HUGE increment
+                    tank.getValve().increment(1-((c * tank.getOutputFlowRate())/Ptot));
                 }
-                         
-                if (tank.getCapacity() < capacityError(true)) {
+
+                log += " - Setting input resistance to " + rj + " - ";
+
+                // OUTPUT SWITCH
+                if (tank.getCapacity() <= capacityError(true)) {                                // ALMOST EMPTY TANK ==>    FORCE NO OUTPUT
                     log += " CLOSING TRIGGER";
                     tank.getTrigger().close();
-                } else if (tank.getCapacity() > capacityError(false)) {
+                } else if (tank.getCapacity() > capacityError(false)) {                         // ALMOST EMPTY TANK ==>    RE-ENABLE OUTPUT
                     log += " OPENING TRIGGER";
                     tank.getTrigger().open();
                 }
-                
+
                 mQTTClientSessionBean.publish(tank.getTankId() + "/", tank);
                 LOG.warning(log);
             }
         });
     }
-    
+
     @Schedule(dayOfMonth = "*", hour = "*", second = "*/" + PERIOD * SECTOR_MULT, minute = "*")
-    private void sectorActuation() { 
-        List<GenericValue> sectors = monitorSessionBean.getSectors(); 
+    private void sectorActuation() {
+        List<GenericValue> sectors = monitorSessionBean.getSectors();
         sectors.forEach((s) -> {
             String log = "";
             if (s instanceof Sector) {
@@ -155,17 +151,17 @@ public class OrchestratorSessionBean implements OrchestratorSessionBeanLocal {
             }
         });
     }
-    
+
     private float flowRateError() {
-        // TODO
+        // TODO : SET ME!
         return 1;
     }
-    
+
     private float capacityError(boolean stak) {
         if (stak == false)
             return (float)(0.15 * VMAX);
         else
             return (float)(0.25 * VMAX);
     }
-    
+
 }
